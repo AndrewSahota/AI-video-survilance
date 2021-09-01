fa4b476becf284bce927de058cf8329d7f6a54b2
AndrewSahota <73869951+AndrewSahota@users.noreply.github.com>
2021-09-01 16:42:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ function draw(){
         for(i=0; i<objects.length;i++){
             document.getElementById("status").innerHTML="Status = Detecting Object";
             document.getElementById("number_of_objects").innerHTML="Number of objects detected are = "+ objects.length;
+            
             fill("#FF0000");
             percent=floor(objects[i].confidence * 100);
             text(objects[i].label+""+percent+"%",objects[i].x+15, objects[i].y+15);
